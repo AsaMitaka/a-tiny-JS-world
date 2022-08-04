9a668772df407f85158574d8683b15ab67441534
AsaMitaka <cepelits@gmail.com>
2022-08-04 15:59:42 +0300

    functional and classes tiny js world

--- index.js
+++ index.js
@@ -7,8 +7,56 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+const dog = {
+   species: 'dog',
+   name: 'Toby',
+   gender: 'male',
+   legs: 4,
+   hands: 0,
+   saying: 'woof-woof!'
+}
 
+const cat = {
+   species: 'cat',
+   name: 'Aimi',
+   gender: 'female',
+   legs: 4,
+   hands: 0,
+   saying: 'meow!'
+}
 
+const man = {
+   species: 'human',
+   name: 'Ihor',
+   gender: 'male',
+   legs: 2,
+   hands: 2,
+   saying: 'Hi!'
+}
+
+const woman = {
+   species: 'human',
+   name: 'Marina',
+   gender: 'female',
+   legs: 2,
+   hands: 2,
+   saying: 'Hello!'
+}
+
+const catWoman = Object.create(cat);
+catWoman.name = 'Cat-Woman';
+catWoman.species = 'human';
+catWoman.legs = 2;
+catWoman.hands = 2;
+catWoman.gender = 'female';
+
+const inhabitants = [dog, cat, man, woman, catWoman];
+
+function aboutObj(obj) {
+   return `${obj.saying} I'm a ${obj.species}. My gender is ${obj.gender}. My name is ${obj.name}. I have ${obj.legs} legs, ${obj.hands} hands`;
+}
+
+inhabitants.forEach(item => print(aboutObj(item)));
 // ======== OUTPUT ========
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
